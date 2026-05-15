d800cabd244677c4371c9b3fa85b1a926d49594b
Paillat7 <ndingatresor38@gmail.com>
2026-05-15 08:35:51 +0100

    Utilisation variable REACT_APP_API_URL pour l'API

--- ofis-frontend/src/services/axiosConfig.js
+++ ofis-frontend/src/services/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
-
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
 
 // Configuration de base
 const axiosInstance = axios.create({
